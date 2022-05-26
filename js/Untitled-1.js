8280762e27d08cc6163901ae13c36b19d9ac25ea
old-ded-insaid <104897212+old-ded-insaid@users.noreply.github.com>
2022-05-26 21:54:40 +0700

    js

---
+++ js/Untitled-1.js
@@ -0,0 +1,38 @@
+document.write("<h1> Hello from js </h1>");
+
+//const age = prompt("сколько лет");
+
+//let access = '';
+
+/*let access = age > 18 ? "добро" : "нет";
+
+/*if(age > 18){
+  access = "добро"
+} else if (age < 18 && age > 14) {
+  access = "еще пару лет бро"
+} else {
+  access = "нет"
+}*/
+/*document.write('<h3>'+access+'</h3>');*/
+
+/*let num = 0;
+/*while (num <= 10) {
+  document.write('<h3>'+num+'</h3>');
+  num = num + 1;
+  if (num == 0) {
+    break;
+  }
+}*/
+
+/*let age = prompt("сколько лет");
+
+while(true) {
+  if (age > 0 && age < 100) {
+    break;
+  }
+  age = prompt('нит')
+}*/
+
+for(let i = 0; i < 10; i++) {
+  document.write('<h3>'+i+'</h3>');
+}
